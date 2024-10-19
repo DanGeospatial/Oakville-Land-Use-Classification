82e69397d40451cba6865c82cf4e14af30d377dd
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-10-18 22:35:50 -0400

    switch to deeplab rn50 still not working because nodata cells are not handled

--- Data/dataset_Oakville_v2.py
+++ Data/dataset_Oakville_v2.py
@@ -3,43 +3,43 @@ This version has fixes for creating tiles and loading satellite images
 Use torchgeo to streamline this task
 """
 import os
-from torchgeo.datasets import RasterDataset, VectorDataset, stack_samples
+import matplotlib.pyplot as plt
+from torchgeo.datasets import RasterDataset, stack_samples
 from torchgeo.samplers import GridGeoSampler, RandomGeoSampler
-from torch.utils.data import random_split, DataLoader
+from torch.utils.data import DataLoader
 
 
 raster_location = '/mnt/d/TWNOakvilleJuly22_23_psscene_analytic_sr_udm2/image/'
-vector_location = '/mnt/d/OakvilleFeatures/'
+mask_location = '/mnt/d/OakvilleMask/mask.tif'
 
 loader_args = dict(num_workers=os.cpu_count(), pin_memory=True)
 
 validation_ratio = 0.20
-test_ratio = 0.20
-batch_size = 20
+batch_size = 5
 
 ps_image = RasterDataset(raster_location)
-oak_mask = VectorDataset(vector_location, label_name="classvalue")
+oak_mask = RasterDataset(mask_location)
+
+oak_mask.is_image = False
 
 # get the intersection of two datasets because ps_image and oak_mask cover slightly different areas
 dataset = ps_image & oak_mask
 
-# Get counts for each split based on dataset length
-test_count, validation_count = int(dataset.__len__() * test_ratio), int(dataset.__len__() * validation_ratio)
-
-# Randomly split the data into train, test, validation
-train, validation, test = random_split(dataset, [(dataset.__len__() - (test_count + validation_count)),
-                                                 test_count, validation_count
-                                                 ])
-
 # sample from ps_image at 256 pixel tiles
-train_sampler = GridGeoSampler(train, size=256, stride=64)
-test_sampler = GridGeoSampler(test, size=256, stride=64)
-valid_sampler = GridGeoSampler(validation, size=256, stride=64)
+train_sampler = GridGeoSampler(ps_image, size=224, stride=64)
+# validation_sampler = GridGeoSampler(dataset , size=256, stride=64)
 
 # Load train, test and validation datasets
-train_set = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, collate_fn=stack_samples, **loader_args)
-validation_set = DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler, collate_fn=stack_samples, **loader_args)
+train_set = DataLoader(dataset , batch_size=batch_size, sampler=train_sampler, collate_fn=stack_samples,
+                       **loader_args)
+# validation_set = DataLoader(dataset, batch_size=batch_size, sampler=validation_sampler, collate_fn=stack_samples,
+# **loader_args)
 
+for batch in train_set:
+    images = batch["image"][0]
+    masks = batch["mask"][0]
+
+    print(masks)
 
 def getLength():
-    return train.__len__()
+    return dataset.__len__()
--- Train.py
+++ Train.py
@@ -1,20 +1,20 @@
 """
 Train the land use dataset with 3 classes and 4 image bands
-Currently using RMSprop instead of adam optimizer
+
 """
 
 # Import PyTorch libraries
 import torch
 import torch.nn as nn
 from torch import device, cuda, optim, autocast, save
-from torch.optim import Optimizer
 
 # Import algorithms
-from Models.R18 import res18
+from Models.deep3 import deeplab
+from Models.Simple_CNN import Net
 from utils.validation_accuracy import evaluate_accuracy
-from Data.dataset_Oakville_v2 import train_set, validation_set, getLength
+from Data.dataset_Oakville_v2 import train_set, getLength
 
-num_classes = 5
+num_classes = 4
 num_bands = 4
 epochs = 40
 learning_rate = 0.01
@@ -42,11 +42,13 @@ def train_model(model, device_hw, epoch_num, lr):
             images = images.to(device=device_hw)
             masks = masks.to(device=device_hw)
 
-            with autocast(device_hw.type):
-                mask_prediction = model(images)
-                loss = criterion(masks, mask_prediction)
+            optimizer.zero_grad()
 
-            Optimizer.zero_grad(set_to_none=True)
+            with autocast(device_hw.type):
+                mask_prediction = model(images)['out']
+                masks = masks.squeeze(1)
+                loss = criterion(mask_prediction, masks)
+
             gradient_scaler.scale(loss).backward()
             nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             gradient_scaler.step(optimizer)
@@ -55,17 +57,17 @@ def train_model(model, device_hw, epoch_num, lr):
             step_glob += 1
             epoch_loss += loss.item()
             print("Train loss: ", loss.item(), "Step: ", step_glob, "epoch: ", epoch)
-
+            """
             # Evaluate Model
             # 5 * batch size
-            step_div = (getLength() // (5 * 10))
+            step_div = (getLength() // (5 * 20))
             if step_div > 0:
                 if step_glob % step_div == 0:
                     validation_score = evaluate_accuracy(model, validation_set, device_hw)
                     scheduler.step(validation_score)
 
                     print("Validation Score: ", validation_score)
-
+            """
     print("Training Complete!")
     state_dict = model.state_dict()
     save(state_dict, out_path)
@@ -74,13 +76,11 @@ def train_model(model, device_hw, epoch_num, lr):
 
 if __name__ == '__main__':
     print("Using PyTorch version: ", torch.__version__)
-    print("With CUDA version: ", torch.cuda_version)
 
     device = device('cuda' if cuda.is_available() else 'cpu')
     print(f"Training on {device}")
 
-    network = res18(num_classes)
-    network = network.to(device)
+    network = deeplab(num_classes, num_bands, device)
 
     try:
         train_model(
